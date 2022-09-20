7d7403b62b47323d283b8315694f4a7c10659ee3
YijieWang <yijwang@thoughtworks.com>
2022-09-20 20:32:07 +0800

    3. 无重复字符的最长子串

---
+++ src/main/java/Solution.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+  public int lengthOfLongestSubstring(String s) {
+    int indexSlow = 0;
+    int indexFast = 0;
+    int longest = 0;
+    Map<Character, Integer> map = new HashMap<>();
+    while (indexFast < s.length()) {
+      char currentChar = s.charAt(indexFast);
+      if (map.containsKey(currentChar)) {
+        longest = Math.max(longest, indexFast - indexSlow);
+        Integer targetPosition = map.get(currentChar);
+        while (indexSlow <= targetPosition) {
+          map.remove(s.charAt(indexSlow));
+          indexSlow++;
+        }
+      }
+      map.put(currentChar, indexFast);
+      indexFast++;
+    }
+    return Math.max(longest, indexFast - indexSlow);
+  }
+}
